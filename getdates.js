f1d97c59d8860b95b054caaebaddf21267c56f4f
Tinny Mosimanyana <mbothepha@gmail.com>
2024-05-18 18:53:48 +0200

    web page enhancement

--- getdates.js
+++ getdates.js
@@ -1,11 +1,8 @@
 // getdates.js
 document.addEventListener("DOMContentLoaded", function() {
-    // Set the current year
-    const currentYearElement = document.getElementById("currentyear");
-    currentYearElement.textContent = new Date().getFullYear();
-  
-    // Set the last modified date
-    const lastModifiedElement = document.getElementById("lastModified");
-    lastModifiedElement.textContent = "Last Modified: " + document.lastModified;
-  });
-  
+  var currentYear = new Date().getFullYear();
+  document.getElementById("currentyear").textContent = currentYear;
+
+  var lastModified = document.lastModified;
+  document.getElementById("lastModified").textContent = "Last modified: " + lastModified;
+});
