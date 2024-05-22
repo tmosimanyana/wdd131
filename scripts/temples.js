d4f6b49466f28d39e8667c00aec2266fcfec94a9
Tinny Mosimanyana <mbothepha@gmail.com>
2024-05-22 10:41:59 +0200

    page updated

--- scripts/temples.js
+++ scripts/temples.js
@@ -1,2 +1,15 @@
-document.getElementById('currentyear').textContent = new Date().getFullYear();
-document.getElementById('lastModified').textContent = 'Last modified: ' + document.lastModified;
+document.addEventListener("DOMContentLoaded", () => {
+    const currentYear = document.getElementById('currentYear');
+    const lastModified = document.getElementById('lastModified');
+    const hamburger = document.querySelector('.hamburger');
+    const navLinks = document.querySelector('.nav-links');
+
+    // Set current year and last modified date
+    currentYear.textContent = new Date().getFullYear();
+    lastModified.textContent = document.lastModified;
+
+    // Toggle navigation menu
+    hamburger.addEventListener('click', () => {
+        navLinks.classList.toggle('active');
+    });
+});
