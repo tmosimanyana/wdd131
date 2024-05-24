f71350ca5b59ba30de0160e4fd182536b5885754
Tinny Mosimanyana <mbothepha@gmail.com>
2024-05-25 00:16:15 +0200

    update code

--- scripts/script.js
+++ scripts/script.js
@@ -1,27 +1,6 @@
-document.addEventListener("DOMContentLoaded", function() {
-    const products = [
-        { id: "fc-1888", name: "flux capacitor" },
-        { id: "fc-2050", name: "power laces" },
-        { id: "fs-1987", name: "time circuits" },
-        { id: "ac-2000", name: "low voltage reactor" },
-        { id: "jj-1969", name: "warp equalizer" }
-    ];
-
-    const productSelect = document.getElementById("product");
-
-    products.forEach(product => {
-        const option = document.createElement("option");
-        option.value = product.id;
-        option.textContent = product.name;
-        productSelect.appendChild(option);
-    });
-
+// JavaScript code for appending the form to the body
+window.onload = function() {
     const form = document.querySelector('form');
-    form.addEventListener('submit', function() {
-        if (localStorage.getItem("reviewCount")) {
-            localStorage.setItem("reviewCount", parseInt(localStorage.getItem("reviewCount")) + 1);
-        } else {
-            localStorage.setItem("reviewCount", 1);
-        }
-    });
-});
+    document.body.appendChild(form);
+};
+
