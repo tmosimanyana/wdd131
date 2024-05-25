42382cb373cd3d5af8a9e121d5de079006fff4dc
Tinny Mosimanyana <mbothepha@gmail.com>
2024-05-25 22:22:56 +0200

    CHANGES STAGED

--- scripts/script.js
+++ scripts/script.js
@@ -1,18 +1,9 @@
-// JavaScript code for dynamically populating the product name select element
-window.onload = function() {
-    const productSelect = document.getElementById('productName');
-    const products = [
-        { id: 'fc-1888', name: 'flux capacitor', averageRating: 4.5 },
-        { id: 'fc-2050', name: 'power laces', averageRating: 4.7 },
-        { id: 'fs-1987', name: 'time circuits', averageRating: 3.5 },
-        { id: 'ac-2000', name: 'low voltage reactor', averageRating: 3.9 },
-        { id: 'jj-1969', name: 'warp equalizer', averageRating: 5.0 }
-    ];
+// scripts/script.js
 
-    products.forEach(function(product) {
-        const option = document.createElement('option');
-        option.value = product.id;
-        option.textContent = product.name;
-        productSelect.appendChild(option);
-    });
-};
+// Footer Copyright Year
+document.getElementById('copyrightYear').textContent = new Date().getFullYear();
+
+// Footer Last Updated Date
+document.getElementById('lastUpdated').textContent = document.lastModified;
+
+
