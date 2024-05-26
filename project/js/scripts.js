7e6b569852108dbfc641205651f4e1221044d81d
Tinny Mosimanyana <mbothepha@gmail.com>
2024-05-26 16:57:13 +0200

    siteplan updated

---
+++ project/js/scripts.js
@@ -0,0 +1,12 @@
+// scripts.js
+
+// This file can be used to add interactive features using JavaScript
+// Example: Smooth scrolling for navigation links
+document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+    anchor.addEventListener('click', function(e) {
+        e.preventDefault();
+        document.querySelector(this.getAttribute('href')).scrollIntoView({
+            behavior: 'smooth'
+        });
+    });
+});
