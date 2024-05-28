7fd9574b4fe2129cc7756c5e7d6461ffea767cc1
Tinny Mosimanyana <mbothepha@gmail.com>
2024-05-28 12:47:00 +0200

    files uploaded

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -1,12 +1,5 @@
 // scripts.js
 
-function showSection(sectionId) {
-    document.querySelectorAll('.main-content').forEach(section => {
-        section.style.display = 'none';
-    });
-    document.getElementById(sectionId).style.display = 'block';
-}
-
 document.addEventListener('DOMContentLoaded', () => {
     document.getElementById('current-year').textContent = new Date().getFullYear();
     document.getElementById('last-modified').textContent = document.lastModified;
