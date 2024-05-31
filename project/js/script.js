0033493e5d5368940c9980669f3723fc35c15dcc
Tinny Mosimanyana <mbothepha@gmail.com>
2024-05-31 20:08:59 +0200

    files for form staged

---
+++ project/js/script.js
@@ -0,0 +1,8 @@
+function toggleMenu() {
+    const menuList = document.getElementById('menu-list');
+    if (menuList.style.display === 'block') {
+        menuList.style.display = 'none';
+    } else {
+        menuList.style.display = 'block';
+    }
+}
