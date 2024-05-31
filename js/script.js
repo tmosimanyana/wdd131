b38cb46d3e0760b2eb114c73f0041b828d2f65bc
Tinny Mosimanyana <mbothepha@gmail.com>
2024-05-31 16:05:03 +0200

    files updated for form

--- js/script.js
+++ js/script.js
@@ -2,7 +2,7 @@ document.addEventListener("DOMContentLoaded", () => {
     const products = [
         { id: 1, name: "Product 1" },
         { id: 2, name: "Product 2" },
-        { id: 3, name: "Product 3" }
+        { id: 3, name: "Product 3" },
         // Add more products as needed
     ];
 
