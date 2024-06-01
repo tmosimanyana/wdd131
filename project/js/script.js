e79adfaeb30bf3f162ea1af098d6ea22451f4a7f
tmosimanyana <162897385+tmosimanyana@users.noreply.github.com>
2024-06-01 19:09:30 +0200

    deleted files

--- project/js/script.js
+++
@@ -1,8 +0,0 @@
-function toggleMenu() {
-    const menuList = document.getElementById('menu-list');
-    if (menuList.style.display === 'block') {
-        menuList.style.display = 'none';
-    } else {
-        menuList.style.display = 'block';
-    }
-}
