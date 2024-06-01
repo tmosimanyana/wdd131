dfbbbbe53efa070cdbb3964bcb9c433800ad88e9
tmosimanyana <162897385+tmosimanyana@users.noreply.github.com>
2024-06-01 14:39:18 +0200

    place file updated

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -10,7 +10,7 @@ document.addEventListener('DOMContentLoaded', () => {
 
     const calculateWindChill = (temp, speed) => {
         if (temp <= 10 && speed > 4.8) {
-            return (13.12 + 0.6215 * temp - 11.37 * Math.pow(speed, 0.16) + 0.3965 * temp * Math.pow(speed, 0.16)).toFixed(2);
+            return ((13.12 + (0.6215 * temp) - (11.37 * Math.pow(speed, 0.16)) + (0.3965 * temp * Math.pow(speed, 0.16)))).toFixed(2);
         }
         return "N/A";
     };
@@ -18,3 +18,4 @@ document.addEventListener('DOMContentLoaded', () => {
     const windChill = calculateWindChill(temperature, windSpeed);
     document.getElementById('wind-chill').textContent = windChill;
 });
+
