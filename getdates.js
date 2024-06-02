01d6c4e1adf578df8ea9fd5def1205d17a9b8a2c
tmosimanyana <162897385+tmosimanyana@users.noreply.github.com>
2024-06-02 15:05:37 +0200

    files uploaded

--- getdates.js
+++ getdates.js
@@ -1,10 +1,16 @@
-document.addEventListener('DOMContentLoaded', (event) => {
-    // Set the current year
-    const currentYearSpan = document.getElementById('currentyear');
+// Function to update the copyright year
+function updateYear() {
+    const yearElement = document.getElementById('year');
     const currentYear = new Date().getFullYear();
-    currentYearSpan.textContent = currentYear;
+    yearElement.textContent = currentYear;
+}
 
-    // Set the last modified date
-    const lastModifiedParagraph = document.getElementById('lastModified');
-    lastModifiedParagraph.textContent = `Last Modified: ${document.lastModified}`;
-});
+// Function to update the last modified date
+function updateLastModified() {
+    const lastModifiedElement = document.getElementById('lastModified');
+    lastModifiedElement.textContent = document.lastModified;
+}
+
+// Call the functions to update the year and last modified date
+updateYear();
+updateLastModified();
