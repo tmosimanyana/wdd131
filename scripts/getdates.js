d2b416a5e5b5aae6d2744dc62caf6f618d50796f
tmosimanyana <162897385+tmosimanyana@users.noreply.github.com>
2024-06-03 22:31:56 +0200

    files updated for home page

--- scripts/getdates.js
+++ scripts/getdates.js
@@ -1,13 +1,15 @@
-// JavaScript File to get dates and modify the HTML dynamically
-
-document.addEventListener("DOMContentLoaded", () => {
-    // Set the current year in the footer
-    const currentYearSpan = document.getElementById('currentyear');
-    const currentYear = new Date().getFullYear();
+document.addEventListener("DOMContentLoaded", function() {
+    // Get the current year
+    var currentYear = new Date().getFullYear();
+    
+    // Update the current year in the footer
+    var currentYearSpan = document.getElementById("currentyear");
     currentYearSpan.textContent = currentYear;
 
-    // Set the last modified date in the footer
-    const lastModifiedSpan = document.getElementById('lastModified');
-    const lastModified = document.lastModified;
-    lastModifiedSpan.textContent = `Last Modified: ${lastModified}`;
+    // Get the last modified date of the document
+    var lastModifiedDate = document.lastModified;
+    
+    // Update the last modified date in the footer
+    var lastModifiedParagraph = document.getElementById("lastModified");
+    lastModifiedParagraph.textContent = "Last Modified: " + lastModifiedDate;
 });
