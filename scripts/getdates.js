7a95a41438559eee3f4114c824cbe789f6e94638
tmosimanyana <162897385+tmosimanyana@users.noreply.github.com>
2024-06-04 21:15:43 +0200

    files updated for homepage

--- scripts/getdates.js
+++ scripts/getdates.js
@@ -1,13 +1,10 @@
-// getdates.js
+document.addEventListener("DOMContentLoaded", () => {
+    // Set the current year
+    const yearSpan = document.getElementById('currentyear');
+    const currentYear = new Date().getFullYear();
+    yearSpan.textContent = currentYear;
 
-// Get the current year
-const currentYear = new Date().getFullYear();
-
-// Update the copyright year in the footer
-document.getElementById('currentyear').innerText = currentYear;
-
-// Get the last modified date of the document
-const lastModified = document.lastModified;
-
-// Update the last modified date in the footer
-document.getElementById('lastModified').innerText = `Last modified: ${lastModified}`;
+    // Set the last modified date
+    const lastModified = document.lastModified;
+    document.getElementById('lastModified').textContent += lastModified;
+});
