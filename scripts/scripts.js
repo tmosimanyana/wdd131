9997ee5fcc713bfcbf32d075332d5d35ac158c92
Tinny Mosimanyana <mbothepha@gmail.com>
2025-09-14 23:49:37 +0200

    refactor: update HTML structure and improve content in index and portfolio pages; add scripts.js for dynamic year and last modified date

---
+++ scripts/scripts.js
@@ -0,0 +1,4 @@
+// scripts/scripts.js
+document.getElementById("current-year").textContent = new Date().getFullYear();
+document.getElementById("last-modified").textContent = "Last Modified: " + document.lastModified;
+
